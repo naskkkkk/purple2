fc7f2e2c63a58eafe199bd89407b6c5b85e013ed
naskkkkk <82781862+naskkkkk@users.noreply.github.com>
2021-04-20 09:31:52 -0300

    Delete schema directory

--- schema/guilddata.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const dataScheama2 = mongoose.Schema({
-    guild:String,
-    guildname:String,
-    guildprefix:String,
-})
-module.exports = mongoose.model("ServerData",dataScheama2)
--- schema/welcome-schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-let Schema = new mongoose.Schema({
-    guildId: String,
-    welcomeMsg: String,
-    channelId: String
-})
-
-module.exports = mongoose.model('welcomes', Schema)
