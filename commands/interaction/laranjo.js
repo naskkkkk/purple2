467d8c8cb20a3dbfd18bac695cce12c61a6d1191
N4SK <82880521+N4SK@users.noreply.github.com>
2021-04-21 04:10:11 -0300

    Create laranjo.js

---
+++ commands/interaction/laranjo.js
@@ -0,0 +1,49 @@
+const jimp = require("jimp")
+const Discord = require('discord.js')
+const cooldowns = {}
+const ms = require("ms")
+
+module.exports = {
+    name: "laranjo",
+    alias: ["laranja"],
+    description: 'laranjo',
+    usage: '<user>',
+async execute (client, message, args){
+
+    if(!cooldowns[message.author.id]) cooldowns[message.author.id] = {
+        lastCmd: null
+      }
+let ultimoCmd = cooldowns[message.author.id].lastCmd 
+     let timeout = 10000
+    if (ultimoCmd !== null && timeout- (Date.now() - ultimoCmd) > 0) {
+let time = ms(timeout - (Date.now() - ultimoCmd)); 
+let resta = [time.seconds, 'segundos']
+ 
+if(resta[0] == 0) resta = ['alguns', 'millisegundos']
+if(resta[0] == 1) resta = [time.seconds, 'segundo']
+const aguarde = new Discord.MessageEmbed()
+  .setTitle(':carregando: Muita Calma nessa hora amigão !!!')
+  .setColor('#F500FF')
+    .setThumbnail('https://cdn.discordapp.com/attachments/755506991019065425/765688678801604659/purple_load.gif')
+  .setDescription(`**Por favor ${message.author}, espere **\`${time}\`** para executar outro comando**`)
+        message.channel.send(` Espere um pouco para utilizar esse comando novamente`).then(msg=> {
+    msg.delete({ timeout: 600000 });
+        })
+       return;
+    } else {
+cooldowns[message.author.id].lastCmd = Date.now() 
+    }
+
+        let img = jimp.read("https://cdn.discordapp.com/attachments/554048737648050179/610011657632219147/laranjo-meme-cke.jpg")
+        if (!args[0]) return message.reply("Escreva algo para o laranjo falar.")
+        img.then(image => {
+            jimp.loadFont(jimp.FONT_SANS_32_BLACK).then(font => {
+                image.resize(685, 494)
+                image.print(font, 20, 30, args.join(" "), 600)
+                image.getBuffer(jimp.MIME_PNG, (err, i) => {
+                    message.channel.send({files: [{ attachment: i, name: "laranjo.png"}]})
+                })
+            })
+        })
+    }
+}
